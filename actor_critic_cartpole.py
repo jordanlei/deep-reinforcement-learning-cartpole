127aca0406136f6b408dd8708a7dab5ba7ffccc1
Jordan Lei <thunderflash.lei@gmail.com>
2020-06-12 23:39:09 -0400

    completed actor critic implementation, updated plot and video saving

--- actor_critic_cartpole.py
+++ actor_critic_cartpole.py
@@ -14,8 +14,6 @@ import time
 from collections import namedtuple
 from itertools import count
 from PIL import Image
-from pyvirtualdisplay import Display
-from IPython import display
 
 import torch
 import torch.nn as nn
@@ -26,9 +24,9 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.autograd import Variable
 from torch.distributions import Categorical
 import ffmpeg
-import seaborn as sns
 
 import argparse
+import seaborn as sns
 import os
 
 parser = argparse.ArgumentParser()
@@ -45,10 +43,6 @@ parser.add_argument('--episodes', type=int, default=500) #number of episodes
 parser.add_argument('--gamma', type=float, default=0.99) #discount factor                                  
 args = parser.parse_args()
 
-#virtual display used to satisfy non-screen evironments (e.g. server)
-# virtualdisplay = Display(visible=0, size=(1400, 900))
-# virtualdisplay.start()
-
 #setup environment
 env = gym.make('CartPole-v0').unwrapped
 
@@ -56,16 +50,13 @@ env = gym.make('CartPole-v0').unwrapped
 os.environ["CUDA_VISIBLE_DEVICES"]= "{}".format(args.device)
 device =  torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-#policy gradient network
-class Policy(nn.Module): 
-  def __init__(self, in_size, out_size):
-    super(Policy, self).__init__()
-    self.in_size = in_size
-    self.out_size = out_size
-
-    self.l1 = nn.Linear(self.in_size, 128)
-    self.l2 = nn.Linear(128, self.out_size)
-    self.dropout = nn.Dropout(0.6)
+#actor network
+class Actor(nn.Module): 
+  def __init__(self, in_size, out_size): 
+    super(Actor, self).__init__()
+    self.linear1 = nn.Linear(in_size, 128)
+    self.linear2 = nn.Linear(128, out_size)
+    self.dropout = nn.Dropout(0.7)
     self.softmax = nn.Softmax(dim= 1)
 
     self.policy_history = Variable(torch.Tensor()).to(device)
@@ -73,106 +64,162 @@ class Policy(nn.Module):
 
     self.reward_history = []
     self.loss_history = []
-
-  def forward(self, x):
-    # convert state to tensor
-
-    x = Variable(torch.from_numpy(x).float().unsqueeze(0)).to(device) 
-    x = self.l1(x)
-    x = F.relu(self.dropout(x))
-    x = self.l2(x)
-    #softmax outputs a probability distribution over action space
-    return self.softmax(x)
-
-class Runner():
-  def __init__(self, net, optimizer, gamma = 0.99, logs = "reinforce_cartpole"):
-    self.net = net
-    self.optimizer = optimizer
-    self.gamma = gamma
-    self.writer = SummaryWriter(logs)
-    self.logs = logs
-    self.plots = {"Loss": [], "Reward": [], "Mean Reward": []}
   
+  def forward(self, x): 
+    #convert numpy state to tensor
+    x = Variable(torch.from_numpy(x).float().unsqueeze(0)).to(device)
+    x = F.relu(self.linear1(x))
+    x = self.dropout(x)
+    x = self.softmax(self.linear2(x))
+    return x
+
+#critic network
+class Critic(nn.Module): 
+  def __init__(self, in_size): 
+    super(Critic, self).__init__()
+    self.linear1 = nn.Linear(in_size, 128)
+    self.linear2 = nn.Linear(128, 1)
+    self.dropout = nn.Dropout(0.7)
+
+    self.value_episode = []
+    self.value_history = Variable(torch.Tensor()).to(device)
+    
+  def forward(self, x): 
+    x = Variable(torch.from_numpy(x).float().unsqueeze(0)).to(device)
+    x = F.relu(self.linear1(x))
+    x = self.linear2(x)
+    return x 
+
+#combined module (mostly for loading / storing)
+class ActorCritic(nn.Module): 
+  def __init__(self, actor, critic): 
+    super(ActorCritic, self).__init__()
+    self.actor = actor
+    self.critic = critic
+  
+  def forward(self, x):
+    value = self.critic(x)
+    policy = self.actor(x)
+
+    return value, policy
+
+class Runner(): 
+  def __init__(self, actor, critic, a_optimizer, c_optimizer, gamma=0.99, logs = "a2c_cartpole"):
+    self.actor = actor
+    self.critic = critic
+    self.a_opt = a_optimizer
+    self.c_opt = c_optimizer
+    self.gamma = gamma
+    self.logs = logs
+    self.writer = SummaryWriter(logs)
+    self.entropy = 0
+    self.plots = {"Actor Loss": [], "Critic Loss": [], "Reward": [], "Mean Reward": []}
+
+  def env_step(self, action):
+    state, reward, done, log = env.step(action)
+    return torch.FloatTensor([state]).to(device), torch.FloatTensor([reward]).to(device), done, log
 
   def select_action(self, state):
     #convert state to tensor
-    probs = self.net(state)
+    probs = self.actor(state)
     c = Categorical(probs)
     action = c.sample()
 
     #place log probabilities into the policy history log\pi(a | s)
-    if self.net.policy_history.dim()!= 0: 
-      self.net.policy_history = torch.cat([self.net.policy_history, c.log_prob(action)])
+    if self.actor.policy_history.dim()!= 0: 
+      self.actor.policy_history = torch.cat([self.actor.policy_history, c.log_prob(action)])
     else: 
-      self.net.policy_history = (c.log_prob(action))
+      self.actor.policy_history = (c.log_prob(action))
     
     return action
   
-  def update_policy(self): 
-    R = 0 
-    rewards = []
-
-    #discount using gamma
-    for r in self.net.reward_episode[::-1]: 
-      R = r + self.gamma * R
-      rewards.insert(0, R)
-    
-    #at this point, rewards is a proxy for Q(s, a)
-    rewards = torch.FloatTensor(rewards)
-    #normalize to reduce variance
-    rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
-
-    #loss = - sum_t log(\pi(a|s)) * v_t
-    loss = torch.sum(torch.mul(self.net.policy_history, Variable(rewards).to(device)).mul(-1), -1)
-    self.optimizer.zero_grad()
-    loss.backward()
-    self.optimizer.step()
-
-    #update reward history and loss histories
-    self.net.loss_history.append(loss.item())
-    self.net.reward_history.append(np.sum(self.net.reward_episode))
-    #flush policy history
-    self.net.policy_history = Variable(torch.Tensor()).to(device)
-    self.net.reward_episode = []
-
-    return loss
+  def estimate_value(self, state): 
+    pred = self.critic(state).squeeze(0)
+    if self.critic.value_history.dim()!= 0: 
+      self.critic.value_history = torch.cat([self.critic.value_history, pred])
+    else: 
+      self.critic.policy_history = (pred)
 
   
-  def train(self, episodes = 200, smooth = 10): 
-    running_reward = 10
-    smoothed_reward = []
-    for episode in range(episodes):
-      state = env.reset()
-      done = False
-      rewards = 0
+  def update_a2c(self):
+    R = 0
+    q_vals = []
 
-      for time in range(500): 
+    #"unroll" the rewards, apply gamma
+    for r in self.actor.reward_episode[::-1]: 
+      R = r + self.gamma * R
+      q_vals.insert(0, R)
+    
+    q_vals = torch.FloatTensor(q_vals).to(device)
+    values = self.critic.value_history
+    log_probs = self.actor.policy_history
+    
+    # print(values)
+    # print(log_probs)
+    advantage = q_vals - values
+  
+    self.c_opt.zero_grad()
+    critic_loss = 0.0005 * advantage.pow(2).mean()
+    critic_loss.backward()
+    self.c_opt.step()
+
+    self.a_opt.zero_grad()
+    actor_loss = (-log_probs * advantage.detach()).mean() + 0.001 * self.entropy
+    actor_loss.backward()
+    self.a_opt.step()
+
+    self.actor.reward_episode = []
+    self.actor.policy_history = Variable(torch.Tensor()).to(device)
+    self.critic.value_history = Variable(torch.Tensor()).to(device)
+    
+  
+    return actor_loss, critic_loss
+  
+  def train(self, episodes = 200, smooth = 10):
+    smoothed_reward = []
+    
+    for episode in range(episodes): 
+      rewards = 0
+      state = env.reset()
+      self.entropy = 0
+      done = False
+
+      for step in range(500): 
+        self.estimate_value(state)
+        
+        policy = self.actor(state).cpu().detach().numpy()
         action = self.select_action(state)
+
+        e = -np.sum(np.mean(policy) * np.log(policy))
+        self.entropy += e
+
         state, reward, done, _ = env.step(action.data[0].item())
         rewards+= reward
 
-        self.net.reward_episode.append(reward)
-        if done: 
-          break
+        self.actor.reward_episode.append(reward)
 
+        if done:
+          break
       
       smoothed_reward.append(rewards)
       if len(smoothed_reward) > smooth: 
         smoothed_reward = smoothed_reward[-1*smooth: -1]
 
-      running_reward = running_reward * self.gamma + time * (1-self.gamma)
-      loss = self.update_policy()
-      self.writer.add_scalar("Loss", loss, episode)
+      a_loss, c_loss = self.update_a2c()
+      
+      self.writer.add_scalar("Critic Loss", c_loss, episode)
+      self.writer.add_scalar("Actor Loss", a_loss, episode)
       self.writer.add_scalar("Reward", rewards, episode)
       self.writer.add_scalar("Mean Reward", np.mean(smoothed_reward), episode)
-      
-      self.plots["Loss"].append(loss)
+
+      self.plots["Critic Loss"].append(c_loss * 100)
+      self.plots["Actor Loss"].append(a_loss)
       self.plots["Reward"].append(rewards)
       self.plots["Mean Reward"].append(np.mean(smoothed_reward))
 
       if episode % 20 == 0: 
-        print("\tEpisode {} \t Final Reward {:.2f} \t Average Reward: {:.2f}".format(episode, rewards, running_reward))
-  
+        print("Episode {} \t Final Reward {:.2f} \t Average Reward: {:.2f}".format(episode, rewards, np.mean(smoothed_reward)))
+
   def run(self):
     sns.set_style("dark")
     sns.set_context("poster")
@@ -191,7 +238,7 @@ class Runner():
     
       im = plt.imshow(env.render(mode='rgb_array'), animated=True)
       plt.axis('off')
-      plt.title("Policy Gradient Agent")
+      plt.title("Actor Critic Agent")
       ims.append([im])
 
     print("\tTotal Reward: ", rewards)
@@ -200,15 +247,20 @@ class Runner():
     ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
                                     repeat_delay=1000)
     ani.save('%s-movie.avi'%self.logs, dpi = 300)
-    # animation.save('animation.gif', writer='PillowWriter', fps=2)
+    
+  def save(self): 
+    ac = ActorCritic(self.actor, self.critic)
+    torch.save(ac.state_dict(),'%s/model.pt'%self.logs)
 
   def plot(self):
     sns.set()
     sns.set_context("poster")
 
     plt.figure(figsize=(20, 16))
-    plt.plot(np.arange(len(self.plots["Loss"])), self.plots["Loss"])
-    plt.title("Policy Gradient Loss")
+    plt.plot(np.arange(len(self.plots["Actor Loss"])), self.plots["Actor Loss"], label = "Actor")
+    plt.plot(np.arange(len(self.plots["Critic Loss"])), self.plots["Critic Loss"], label = "Critic (x100)")
+    plt.legend()
+    plt.title("A2C Loss")
     plt.xlabel("Episodes")
     plt.ylabel("Loss")
     plt.savefig("%s/plot_%s.png"%(self.logs, "loss"))
@@ -217,35 +269,29 @@ class Runner():
     plt.plot(np.arange(len(self.plots["Reward"])), self.plots["Reward"], label="Reward")
     plt.plot(np.arange(len(self.plots["Mean Reward"])), self.plots["Mean Reward"], label = "Mean Reward")
     plt.legend()
-    plt.title("Policy Gradient Rewards")
+    plt.title("A2C Rewards")
     plt.xlabel("Episodes")
     plt.ylabel("Rewards")
     plt.savefig("%s/plot_%s.png"%(self.logs, "rewards"))
-
-    # for key in self.plots.keys():
-    #     data = self.plots[key]
-    #     plt.figure(figsize=(20, 16))
-    #     plt.plot(np.arange(len(data)), data)
-    #     plt.title("Policy Gradient %s"%key)
-    #     plt.xlabel("Episodes")
-    #     plt.ylabel(key)
-    #     plt.savefig("%s/plot_%s.png"%(self.logs, key))
-
-  def save(self): 
-    torch.save(self.net.state_dict(),'%s/model.pt'%self.logs)
 
 def main(): 
     device_name = "cuda: %s"%(args.device) if torch.cuda.is_available() else "cpu"
     print("[Device]\tDevice selected: ", device_name)
 
-    policy = Policy(env.observation_space.shape[0], env.action_space.n).to(device)
+    actor = Actor(env.observation_space.shape[0], env.action_space.n).to(device)
+    critic = Critic(env.observation_space.shape[0]).to(device)
+    ac = ActorCritic(actor, critic)
     
     #if we're loading a model
     if args.load: 
-        policy.load_state_dict(torch.load(args.model))
+        ac.load_state_dict(torch.load(args.model))
+        actor = ac.actor
+        critic = ac.critic
 
-    optimizer = optim.Adam(policy.parameters(), lr = args.lr)
-    runner = Runner(policy, optimizer, gamma = args.gamma, logs = "reinforce_cartpole/%s" %time.time())
+    a_optimizer = optim.Adam(actor.parameters(), lr = args.lr)
+    c_optimizer = optim.Adam(critic.parameters(), lr = args.lr)
+
+    runner = Runner(actor, critic, a_optimizer, c_optimizer, logs = "a2c_cartpole/%s" %time.time())
     
     if "train" in args.runtype:
         print("[Train]\tTraining Beginning ...")
@@ -258,8 +304,7 @@ def main():
     if args.save: 
         print("[Save]\tSaving Model ...")
         runner.save()
-    
-    
+
     if "run" in args.runtype:
         print("[Run]\tRunning Simulation ...")
         runner.run()
